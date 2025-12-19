3644e8caf375827348af57a8050b070b97cf01e3
Joshua T <joshua@sonofone.net>
2025-12-19 09:50:29 -0600

    style: run alejandra formatter

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,15 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [pkgs.git];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
